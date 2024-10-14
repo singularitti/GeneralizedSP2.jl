13355210e8865e83fef2b7afe75cf1edc76672cd
singularitti <singularitti@outlook.com>
2024-10-14 16:57:14 -0600

    Stage results

--- examples/distributions.jl
+++ examples/distributions.jl
@@ -55,25 +55,28 @@ set_isapprox_rtol(1e-13)
 matsize = 1000
 dist = Cauchy(0.35, 0.2)
 # dist = Arcsine(0.2, 0.9)
-# dist = Beta(2, 2)
+dist = Erlang(5, 1)
+dist = JohnsonSU(0, 1, 0, 1)
 # dist = BetaPrime(1, 2)
 # dist = Exponential(1)
 # dist = Laplace(0.5, 0.1)
-# dist = LogitNormal(0, 1)
+# dist = LogitNormal(-5, 7)
 # dist = LogUniform(0.1, 0.9)
-# dist = Uniform(0, 0.8)
+# dist = Uniform(-5, 7)
 # dist = MixtureModel([Normal(0.2, 0.1), Normal(0.5, 0.1), Normal(0.9, 0.1)], [0.3, 0.4, 0.3])
 # dist = MixtureModel([Cauchy(0.25, 0.2), Laplace(0.5, 0.1)], [0.6, 0.4])
-dist = MixtureModel(
-    [Uniform(0, 0.2), Uniform(0.2, 0.5), Uniform(0.5, 0.7), Uniform(0.7, 1)],
-    [0.1, 0.2, 0.2, 0.5],
-)
+# dist = MixtureModel(
+#     [Uniform(0, 0.2), Uniform(0.2, 0.5), Uniform(0.5, 0.7), Uniform(0.7, 1)],
+#     [0.1, 0.2, 0.2, 0.5],
+# )
 Λ = rand(EigvalsSampler(dist), matsize)
 V = rand(EigvecsSampler(dist), matsize, matsize)
 H = Hamiltonian(Eigen(Λ, V))
 # H = diagonalhamil(matsize, 100)
 emin, emax = eigvals_extrema(H)
-𝐱 = rescale_zero_one(emin, emax).(sort(eigvals(H)))  # Cannot do `sort(eigvals(Hinput))` because it is reversed!
+# 𝐱 = rescale_zero_one(emin, emax).(sort(eigvals(H)))  # Cannot do `sort(eigvals(Hinput))` because it is reversed!
+lower_bound, upper_bound = 0, 1
+𝐱 = sample_by_pdf(bell_distribution(μ, β), μ, (lower_bound, upper_bound))
 H_scaled = rescale_zero_one(emin, emax)(H)
 𝐲̂ = fermi_dirac.(𝐱, μ, β)
 dm_exact = fermi_dirac(H_scaled, μ, β)
@@ -89,6 +92,7 @@ estimated_mu = []
 for nlayers in layers
     𝛉 = fit_fermi_dirac(𝐱, μ, β, nlayers)
     𝐲 = fermi_dirac_model(𝐱, 𝛉)
+    # 𝐲 = 𝐲 .* (emin - emax) .+ emax
     𝝝̄ = manualdiff_model(transform_fermi_dirac_derivative, 𝐱, 𝛉)
     dm = fermi_dirac_model(H_scaled, 𝛉)
     Nocc = tr(dm)
@@ -131,7 +135,14 @@ ylabel!(raw"$| \dot{\theta} |$"; subplot=3)
 ylabel!(raw"$\mu$"; subplot=4)
 
 for (𝐲, nlayer) in zip(ys, layers)
-    plot!(𝐱, 𝐲; subplot=5, linestyle=:dot, legend_position=:left, label="$nlayer layers")
+    plot!(
+        𝐱 .* (emin - emax) .+ emax,
+        𝐲;
+        subplot=5,
+        linestyle=:dot,
+        legend_position=:left,
+        label="$nlayer layers",
+    )
 end
 # xlims!(0, 1; subplot=5)
 xlabel!("eigenvalues distribution"; subplot=5)
@@ -145,8 +156,8 @@ histogram!(
     legend_position=:top,
     label="diagonalized eigenvalues distribution",
 )
-histogram!(Λ; subplot=6, nbins=nbins, normalize=true, label="original random eigvals")
-plot!(truncated(dist; lower=0, upper=1); subplot=6, label="original distribution")
+# histogram!(Λ; subplot=6, nbins=nbins, normalize=true, label="original random eigvals")
+# plot!(truncated(dist; lower=0, upper=1); subplot=6, label="original distribution")
 # xlims!(0, 1; subplot=6)
 xlabel!("eigenvalues distribution"; subplot=6)
 ylabel!("density"; subplot=6)
