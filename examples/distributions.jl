b6090a260a3267bcc675ec6d84fb176ca18d3e68
singularitti <singularitti@outlook.com>
2024-10-14 03:07:12 -0600

    Update examples/distributions.jl

--- examples/distributions.jl
+++ examples/distributions.jl
@@ -19,7 +19,7 @@ PLOT_DEFAULTS = Dict(
     :guidefontsize => 9,
     :tickfontsize => 7,
     :legendfontsize => 7,
-    :left_margin => (0, :mm),
+    :left_margin => (1, :mm),
     :grid => nothing,
     :legend_foreground_color => nothing,
     :legend_background_color => nothing,
@@ -27,6 +27,26 @@ PLOT_DEFAULTS = Dict(
     :background_color_inside => nothing,
     :color_palette => :tab10,
 )
+
+function estimate_mu(𝐇, Nocc)
+    Nocc = floor(Int, Nocc)
+    diagonal = sort(diag(𝐇))
+    HOMO, LUMO = diagonal[Nocc], diagonal[Nocc + 1]
+    μ₀ = (HOMO + LUMO) / 2
+    g(μ) = Nocc - sum(fermi_dirac.(diagonal, μ, β))
+    g′(μ) = sum(fermi_dirac_prime.(diagonal, μ, β))
+    return find_zero((g, g′), μ₀, Newton(); atol=1e-8, maxiters=50, verbose=true)
+end
+
+function compute_mu(𝐇, Nocc)
+    Nocc = floor(Int, Nocc)
+    evals = eigvals(𝐇)
+    HOMO, LUMO = evals[Nocc], evals[Nocc + 1]
+    μ₀ = (HOMO + LUMO) / 2
+    g(μ) = Nocc - sum(fermi_dirac.(evals, μ, β))
+    g′(μ) = sum(fermi_dirac_prime.(evals, μ, β))
+    return find_zero((g, g′), μ₀, Newton(); atol=1e-8, maxiters=50, verbose=true)
+end
 
 set_isapprox_rtol(1e-13)
 matsize = 1000
@@ -36,10 +56,10 @@ matsize = 1000
 # dist = Exponential(1)
 # dist = Laplace(0.5, 0.1)
 # dist = LogitNormal(0, 1)
-dist = LogUniform(0.1, 0.9)
+# dist = LogUniform(0.1, 0.9)
 # dist = Uniform(0, 0.8)
 # dist = MixtureModel([Normal(0.2, 0.1), Normal(0.5, 0.1), Normal(0.9, 0.1)], [0.3, 0.4, 0.3])
-# dist = MixtureModel([Cauchy(0.25, 0.2), Laplace(0.5, 0.1)], [0.6, 0.4])
+dist = MixtureModel([Cauchy(0.25, 0.2), Laplace(0.5, 0.1)], [0.6, 0.4])
 # dist = MixtureModel(
 #     [Uniform(0, 0.2), Uniform(0.2, 0.5), Uniform(0.5, 0.7), Uniform(0.7, 1)],
 #     [0.1, 0.2, 0.2, 0.5],
@@ -52,7 +72,7 @@ dist = LogUniform(0.1, 0.9)
 H = diagonalhamil(matsize, 100)
 
 β = 4
-μ = 0.8
+μ = 0.5
 emin, emax = eigvals_extrema(H)
 𝐱 = rescale_zero_one(emin, emax).(sort(eigvals(H)))  # Cannot do `sort(eigvals(Hinput))` because it is reversed!
 𝐲̂ = fermi_dirac.(𝐱, μ, β)
@@ -68,12 +88,16 @@ Nocc_record = []
 diff_norms = []
 diff_Nocc = []
 derivative_norms = []
+estimated_mu = []
+computed_mu = []
 plot(; layout=(2, 2), PLOT_DEFAULTS...)
 for nlayers in layers
     𝛉 = fit_fermi_dirac(𝐱, μ, β, nlayers)
     𝝝̄ = manualdiff_model(transform_fermi_dirac_derivative, 𝐱, 𝛉)
     dm = fermi_dirac_model(H_scaled, 𝛉)
     Nocc = tr(dm)
+    push!(estimated_mu, estimate_mu(H_scaled, Nocc))
+    push!(computed_mu, compute_mu(H_scaled, Nocc))
     push!(Nocc_record, Nocc)
     push!(diff_norms, norm(dm_exact - dm))
     push!(diff_Nocc, N_exact - Nocc)
@@ -81,10 +105,20 @@ for nlayers in layers
 end
 scatter!(layers, diff_norms; subplot=1, xticks=layers, label="")
 scatter!(layers, diff_Nocc; subplot=3, xticks=layers, label="")
-scatter!(layers, derivative_norms; subplot=4, xticks=layers, label="")
+# scatter!(layers, derivative_norms; subplot=4, xticks=layers, label="")
+hline!([μ]; subplot=4, xticks=layers, label="original μ")
+scatter!(
+    layers, estimated_mu; subplot=4, markershape=:diamond, xticks=layers, label="estimatd μ"
+)
+scatter!(
+    layers, computed_mu; subplot=4, markershape=:circle, xticks=layers, label="computed μ"
+)
 xlabel!(raw"number of layers $L$"; subplot=1)
+xlabel!(raw"number of layers $L$"; subplot=3)
+xlabel!(raw"number of layers $L$"; subplot=4)
 ylabel!(raw"$| D - P |^2$"; subplot=1)
 ylabel!(raw"$N_0 - N$"; subplot=3)
+ylabel!(raw"$\mu$"; subplot=4)
 
 histogram!(
     eigvals(H); subplot=2, nbins=nbins, normalize=true, label="solve the Hamiltonian"
