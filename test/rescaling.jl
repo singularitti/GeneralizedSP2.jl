83c6c86a7837a1d473f5eb5cbeafa37afc6eb445
singularitti <singularitti@outlook.com>
2024-10-31 01:27:42 -0600

    Add "Test on the rescaled Fermi–Dirac function" in test/rescaling.jl

--- test/rescaling.jl
+++ test/rescaling.jl
@@ -8,6 +8,11 @@ function rand_hamiltonian(syssize, α=2)
     return collect(hermitianpart(Q * Λ * Q')), 𝛌  # Use `collect` to avoid Hermitian-specific algorithms
 end
 
+function rescaled_hamiltonian(H::AbstractMatrix)
+    εₘᵢₙ, εₘₐₓ = minimum(eigvals(H)) - 10, maximum(eigvals(H)) + 10
+    return rescale_one_zero(εₘᵢₙ, εₘₐₓ)(H), εₘᵢₙ, εₘₐₓ
+end
+
 @testset "Test `eigvals` will return a random order of eigenvalues" begin
     H, 𝛌 = rand_hamiltonian(1024, 0.54)
     @assert ishermitian(H)
@@ -18,5 +23,13 @@ end
         D = fermi_dirac(H, μ, β)
         @test !≈(eigvals(D), fermi_dirac.(eigvals(H), μ, β))  # Not sorted
         @test sort(eigvals(D)) ≈ fermi_dirac.(reverse(sort(eigvals(H))), μ, β)  # We need to reverse it since it is Fermi–Dirac
+    end
+    @testset "Test on the rescaled Fermi–Dirac function" begin
+        μ, β = 0.35, 4
+        H_scaled, εₘᵢₙ, εₘₐₓ = rescaled_hamiltonian(H)
+        D = rescaled_fermi_dirac2(H, μ, β, (εₘᵢₙ, εₘₐₓ))
+        D′ = rescaled_fermi_dirac(H, μ, β, (εₘᵢₙ, εₘₐₓ))
+        @test D ≈ D′
+        @test eigvals(D) ≈ eigvals(D′)
     end
 end
