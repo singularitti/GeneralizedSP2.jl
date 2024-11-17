aecbdbc8f1db0490183173541144d50e0729f70b
singularitti <singularitti@outlook.com>
2024-11-17 03:11:00 -0700

    Rename src/models.jl → src/models.jl → src/primals.jl

--- src/GeneralizedSP2.jl
+++ src/GeneralizedSP2.jl
@@ -5,7 +5,7 @@ const LAYER_WIDTH = 4
 include("rescaler.jl")
 include("stat_mech.jl")
 include("types.jl")
-include("models.jl")
+include("primals.jl")
 include("diff.jl")
 include("fitting.jl")
 include("sp2.jl")
