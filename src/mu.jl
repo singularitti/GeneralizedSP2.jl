840db0b6ba28fb891b15ed81049376fa10381d5f
singularitti <singularitti@outlook.com>
2024-11-21 23:54:28 -0700

    Rename variables in `newton_raphson_iteration` & `estimate_mu` in src/mu.jl

--- src/mu.jl
+++ src/mu.jl
@@ -2,9 +2,9 @@ using LinearAlgebra: tr
 
 export newton_raphson_iteration, estimate_mu
 
-function newton_raphson_iteration(DM, β, occupation₀; occ_atol=1e-7)
+function newton_raphson_iteration(DM, β, target_occupation; occ_atol=1e-7)
     occupation = tr(DM)
-    occupation_error = occupation₀ - occupation
+    occupation_error = target_occupation - occupation
     derivatives = fermi_dirac_deriv(DM, β)
     Δμ′ = occupation_error / tr(derivatives)
     return occupation_error > occ_atol ? Δμ′ : zero(Δμ′)
@@ -14,8 +14,8 @@ function estimate_mu(
     H,
     𝐱′,
     β,
-    occupation₀,
-    μ=sum(extrema(H)) / 2,
+    target_occupation,
+    μ_init=sum(extrema(H)) / 2,
     𝛆=extrema(H),
     nlayers=20;
     max_iter=1000,
@@ -24,12 +24,12 @@ function estimate_mu(
 )
     H′ = rescale_one_zero(𝛆)(H)
     β′ = rescale_beta(β, 𝛆)
-    μ′ = rescale_mu(μ, 𝛆)
+    μ′ = rescale_mu(μ_init, 𝛆)
     Δμ′ = oneunit(μ′)  # Initialize with a non-zero value
     while !iszero(Δμ′)
         M = fit_fermi_dirac(𝐱′, μ′, β′, nlayers; max_iter=max_iter, kwargs...).model
         DM = fermi_dirac(M)(H′)
-        Δμ′ = newton_raphson_iteration(DM, β, occupation₀; occ_atol=occ_atol)
+        Δμ′ = newton_raphson_iteration(DM, β, target_occupation; occ_atol=occ_atol)
         μ′ += Δμ′
     end
     return μ′
