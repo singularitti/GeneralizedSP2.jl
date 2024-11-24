9b750ead4d2bc13d850c288edce2844f1ae5cc3b
singularitti <singularitti@outlook.com>
2024-11-24 01:26:43 -0700

    Fix examples/mu.jl

--- examples/mu.jl
+++ examples/mu.jl
@@ -5,22 +5,26 @@ using ToyHamiltonians
 
 β = 1.25
 μ = 100
-H = tridiagonalhamil(1000, 235, 235)
+H = tridiagonalhamil(1000, 235, 400)
 εₘᵢₙ, εₘₐₓ = eigvals_extrema(H)
 β′ = rescale_beta(β, (εₘᵢₙ, εₘₐₓ))
 μ′ = rescale_mu(μ, (εₘᵢₙ, εₘₐₓ))
 H_scaled = rescale_one_zero(εₘᵢₙ, εₘₐₓ)(H)
+nlayers = 18
 
 lower_bound, upper_bound = 0, 1
 𝛆′ = chebyshevnodes_1st(500, (lower_bound, upper_bound))
-fitted = fit_fermi_dirac(𝛆′, μ′, β′, 18; max_iter=1_00_000);
+fitted = fit_fermi_dirac(𝛆′, μ′, β′, nlayers; max_iter=100_000);
 dm = fermi_dirac(fitted.model)(H_scaled)
 N = tr(dm)
 
 N_target = N + 100
 μ_init = μ + 50
-μ′_final = estimate_mu(N_target, H, β, 𝛆′, (εₘᵢₙ, εₘₐₓ), μ_init; fitting_max_iter=1_000_000)
-fitted_final = fit_fermi_dirac(𝛆′, μ′_final, β′, 18; max_iter=1_000_000);
+μ′_history = estimate_mu(
+    N_target, H, β, 𝛆′, (εₘᵢₙ, εₘₐₓ), μ_init, nlayers; fitting_max_iter=10000
+)
+μ′_final = μ′_history[end]
+fitted_final = fit_fermi_dirac(𝛆′, μ′_final, β′, nlayers; max_iter=1_000_000);
 dm_final = fermi_dirac(fitted_final.model)(H_scaled)
 N_final = tr(dm_final)
 @show diff = N_final - N_target
