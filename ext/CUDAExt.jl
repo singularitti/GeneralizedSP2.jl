dedb59a38afec67e8e1f5b1ae9083f4cf04afac1
singularitti <singularitti@outlook.com>
2024-12-07 01:00:58 -0700

    Rename variables according to CUSOLVER API

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -13,26 +13,24 @@ using LinearAlgebra: Eigen, checksquare
 
 import GeneralizedSP2: fermi_dirac
 
-function diagonalize!(E::Eigen, H::CuMatrix)
+function diagonalize!(E::Eigen, H::CuMatrix{Cdouble})
     checksquare(H)
-    dim = size(H, 1)
+    N = size(H, 1)
     evals, evecs = E  # Destructuring via iteration
     # Create cuSOLVER handle
     cusolver_handle = Ref{cusolverDnHandle_t}(C_NULL)
     cusolverDnCreate(cusolver_handle)
     # Specify cuSOLVER diag flags
-    jobz = CUSOLVER_EIG_MODE_VECTOR  # Compute eigenvectors
+    jobz = CUSOLVER_EIG_MODE_VECTOR  # Compute both singular values and singular vectors
     uplo = cublasFillMode_t(0)      # CUBLAS_FILL_MODE_LOWER
     # Determine the buffer size required
     lwork = Ref{Cint}(0)
-    cusolverDnDsyevd_bufferSize(cusolver_handle[], jobz, uplo, dim, H, dim, evals, lwork)
+    cusolverDnDsyevd_bufferSize(cusolver_handle[], jobz, uplo, N, H, N, evals, lwork)
     # Allocate temporary workspace and device info array
-    d_work = CuArray{Float64}(undef, lwork[])
-    devInfo = CuArray{Cint}(undef, 1)
+    work = CuMatrix{Cdouble}(undef, lwork[])
+    devInfo = CuVector{Cint}(undef, 1)
     # Diagonalize the matrix
-    cusolverDnDsyevd(
-        cusolver_handle[], jobz, uplo, dim, H, dim, evals, d_work, lwork[], devInfo
-    )
+    cusolverDnDsyevd(cusolver_handle[], jobz, uplo, N, H, N, evals, work, lwork[], devInfo)
     # Copy the eigenvectors to GPU_eigvecs
     copyto!(evecs, H)
     # Clean up resources
@@ -41,8 +39,8 @@ function diagonalize!(E::Eigen, H::CuMatrix)
 end
 function diagonalize(H::CuMatrix)
     dim = size(H, 1)
-    evals = CuVector{Float64}(undef, dim)
-    evecs = CuMatrix{Float64}(undef, dim, dim)
+    evals = CuVector{Cdouble}(undef, dim)
+    evecs = CuMatrix{Cdouble}(undef, dim, dim)
     return diagonalize!(Eigen(evals, evecs), H)
 end
 
