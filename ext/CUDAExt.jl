2003d85d84a4ba47ab346205bdf738758d5943c8
singularitti <singularitti@outlook.com>
2024-12-07 03:03:09 -0700

    Fix `diagonalize!` modifies `H` itself

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -23,6 +23,8 @@ function diagonalize!(
     if M != N  # See https://github.com/JuliaLang/LinearAlgebra.jl/blob/d2872f9/src/LinearAlgebra.jl#L300-L304
         throw(DimensionMismatch(lazy"matrix is not square: dimensions are $(size(A))"))
     end
+    H′ = similar(H)        # Allocate a new CuMatrix on the device
+    copyto!(H′, H)         # Efficiently copy data from H to H_copy on the device
     # Create cuSOLVER handle
     cusolver_handle = Ref{cusolverDnHandle_t}(C_NULL)
     cusolverDnCreate(cusolver_handle)
@@ -31,12 +33,12 @@ function diagonalize!(
     uplo = convert(cublasFillMode_t, 'L')  # CUBLAS_FILL_MODE_LOWER, see https://github.com/JuliaGPU/CUDA.jl/blob/45571e9/lib/cublas/util.jl#L49-L57
     # Determine the buffer size required
     lwork = Ref{Cint}(0)
-    cusolverDnDsyevd_bufferSize(cusolver_handle[], jobz, uplo, N, H, N, evals, lwork)
+    cusolverDnDsyevd_bufferSize(cusolver_handle[], jobz, uplo, N, H′, N, evals, lwork)
     # Allocate temporary workspace and device info array
     work = CuVector{Cdouble}(undef, lwork[])
     devInfo = CuVector{Cint}(undef, 1)
     # Diagonalize the matrix
-    cusolverDnDsyevd(cusolver_handle[], jobz, uplo, N, H, N, evals, work, lwork[], devInfo)
+    cusolverDnDsyevd(cusolver_handle[], jobz, uplo, N, H′, N, evals, work, lwork[], devInfo)
     # Handle errors
     retcode = only(Vector(devInfo))  # Copy memory from the GPU
     if retcode < 0
@@ -49,7 +51,7 @@ function diagonalize!(
         )
     end
     # Copy the eigenvectors to GPU_eigvecs
-    copyto!(evecs, H)
+    copyto!(evecs, H′)
     # Clean up resources
     cusolverDnDestroy(cusolver_handle[])
     return evals, evecs
