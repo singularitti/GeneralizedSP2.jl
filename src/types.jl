a02107f1342a637275dcfde5f282ec3610773a52
singularitti <singularitti@outlook.com>
2025-01-25 21:30:42 -0700

    Fix constructors of `Model`

--- src/types.jl
+++ src/types.jl
@@ -5,13 +5,19 @@ export Model, numlayers, eachlayer
 struct Model{T} <: AbstractVectorOfSimilarVectors{T}
     data::VectorOfSimilarVectors{T}
 end
-Model(A::AbstractMatrix) = Model(collect(Iterators.partition(A, LAYER_WIDTH)))
+function Model(A::AbstractMatrix)
+    if size(A, 1) != LAYER_WIDTH
+        throw(DimensionMismatch("model matrix must have $LAYER_WIDTH rows!"))  # See https://discourse.julialang.org/t/120556/2
+    end
+    return Model(eachcol(A))
+end
 function Model(A::AbstractVector{<:AbstractVector})
     if innersize(A) != (LAYER_WIDTH,)
         throw(DimensionMismatch("invalid dimensions for `Model`!"))  # See https://discourse.julialang.org/t/120556/2
     end
     return Model(VectorOfSimilarVectors(A))
 end
+Model(A::AbstractVector) = Model(collect(Iterators.partition(A, LAYER_WIDTH)))
 Model(model::Model) = model
 
 numlayers(model::Model) = size(model)
