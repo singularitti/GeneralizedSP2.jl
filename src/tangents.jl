47c79067ddfe76e523c6719173c1804900235f62
singularitti <singularitti@outlook.com>
2026-08-19 02:32:26 -0500

    Revert `autodiff_model!(f, grad, model, x, backend)` to old definition, redirect Enzyme.jl implementation to EnzymeExt module

--- src/tangents.jl
+++ src/tangents.jl
@@ -22,13 +22,8 @@ function autodiff_model!(f, grad, model, x, backend)
     if length(grad) != length(model)
         throw(DimensionMismatch("the length of gradient and the model are not equal!"))
     end
-    # Enzyme requires the primal and gradient buffers to have the same concrete
-    # array type, so differentiate plain storage and copy the result to `grad`.
-    parameters = collect(model)
-    gradient_buffer = similar(parameters)
-    g(parameters′) = f(FlatModel(parameters′)(x))
-    gradient!(g, gradient_buffer, backend, parameters)
-    return copyto!(grad, gradient_buffer)
+    g(model′) = f(model′(x))
+    return gradient!(g, grad, backend, model)
 end
 
 function manualdiff_model(f′, model, x)
